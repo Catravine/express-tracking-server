c41c89e7f3c2db9a555fb455193bc400780ef628
Catravine <caroline@carolinecourtney.com>
2023-09-04 14:47:17 -0400

    hashed and salted passwords with bcrypt

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -17,4 +17,22 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide an email and password' });
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).send({ error: 'Invalid Email or Password' });
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+    res.send({ token })
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid Email or Password' })
+  }
+});
+
 module.exports = router;
